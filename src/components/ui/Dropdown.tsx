0fb520ee4984fd27bf695013224a2e363f2e1612
Sadik Saifi <Sadiksaifi205@gmail.com>
2023-08-02 18:05:27 +0530

    fix: update navbar dropdown data

--- src/components/ui/Dropdown.tsx
+++ src/components/ui/Dropdown.tsx
@@ -12,17 +12,27 @@ export interface IDropdownProps {
 const Dropdown: FC<IDropdownProps> = ({ title, to, subLabel }) => {
   const [isDropDownOpen, setIsDropDownOpen] = useState<boolean>(false)
 
+  const handleLinkMouseLeave = () => {
+    // Delay closing the dropdown to check if the mouse has entered the dropdown content
+    setTimeout(() => {
+      if (isDropDownOpen) {
+        setIsDropDownOpen(false)
+      }
+    }, 200);
+  }
+
   return (
     <>
       <Link
         className='relative'
         to={to}
         onMouseEnter={() => setIsDropDownOpen(true)}
+        onMouseLeave={handleLinkMouseLeave}
       >
         {title}
       </Link>
       <div
-        onMouseLeave={() => setIsDropDownOpen(false)}
+        id="dropdown"
         className={`transition-opacity ease-in-out delay-100 absolute pt-6 bg-white tracking-widest font-medium text-xs text-neutral-600 h-[450px] border-b-[1px] w-[100vw] left-0 px-[22rem] flex justify-between ${
           isDropDownOpen ? "top-20" : "top-[-100%]"
         }`}
